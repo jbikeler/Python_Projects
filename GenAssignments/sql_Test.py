06c075832fc49ca29ab0982d34b7169718cec8b9
jbikeler <jbikeler@gmail.com>
2023-07-19 14:06:30 -0700

    Added File Transfer

--- GenAssignments/sql_Test.py
+++ GenAssignments/sql_Test.py
@@ -64,7 +64,7 @@ import sqlite3
 
 
 #--------Assignment 8--------
-c.execute ("SELECT FirstName,LastName FROM People WHERE Age > 30")
+c.execute("SELECT FirstName,LastName FROM People WHERE Age > 30")
 while True:
     row = c.fetchone()
     if row is None:
